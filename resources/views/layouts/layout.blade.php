a8f4e8070f980aaa5b1bc1ed21795fb12754d768
MIJanda <mokiliisaacjanda@gmail.com>
2022-08-19 20:33:37 +0300

    adds query params

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -18,8 +18,9 @@
 
     </head>
     <body class="antialiased">
-        @yield('content')
-
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+            @yield('content')
+        </div>
         <footer>
             Copyright 2020 Pizza House
         </footer>
