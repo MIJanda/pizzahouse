eb2e26eb07532246cf7b474965bcf567dd9c0215
MIJanda <mokiliisaacjanda@gmail.com>
2022-08-20 18:16:27 +0300

    adds a Pizza model

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,16 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pizza;
 
 class PizzaController extends Controller
 {
     public function index()
     {
-        $pizzas = [
-            ['type' => 'hawaiian', 'base' => 'cheesy crust'],
-            ['type' => 'volcano', 'base' => 'garlic crust'],
-            ['type' => 'veg supreme', 'base' => 'thin & crispy'],
-        ];
+        /** Retrieve pizzas from database */
+        $pizzas = Pizza::all();
+        // $pizzas = Pizza::orderBy('name', 'desc')->get();
+        // $pizzas = Pizza::latest()->get();
+        // $pizzas = Pizza::where('type', 'hawaiian')->get();
     
         return view('pizzas', [
             'pizzas' => $pizzas, 
